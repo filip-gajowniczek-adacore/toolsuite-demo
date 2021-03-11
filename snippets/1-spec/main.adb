8040018073be8d7e132588cb69f4d324cc7948ad
unknown <gajowniczek@adacore.com>
2021-03-11 16:57:53 -0600

    Improvements to directory naming, archival of the final state

--- snippets/1-spec/main.adb
+++ snippets/1-spec/main.adb
@@ -1,10 +1,12 @@
+with Ada.Text_IO; use Ada.Text_IO;
 with Controller; use Controller;
 
 procedure Main is
 
 begin
+
    -- The ada code will have the logic for issues commands to change the power level
    -- It will poll for status from the device, and then based on the status
    -- It will do state transitions.
-   null;
+   Put_Line("Initializing Device");
 end Main;
